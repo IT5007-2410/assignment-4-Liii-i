f6ea14075272826bec0c8d9cbe6e050ed40288f3
Liana <liana@LianadeMacBook-Air.local>
2024-11-17 20:50:33 +0800

    Q2

--- ReactNativeClient/IssueList.js
+++ ReactNativeClient/IssueList.js
@@ -72,22 +72,33 @@ class IssueFilter extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
   header: { height: 50, backgroundColor: '#537791' },
-  text: { textAlign: 'center' },
+  text: { textAlign: 'center', color: '#fff' },
   dataWrapper: { marginTop: -1 },
   row: { height: 40, backgroundColor: '#E7E6E1' }
-  });
+});
+
 
 const width= [40,80,80,80,80,80,200];
 
 function IssueRow(props) {
     const issue = props.issue;
     {/****** Q2: Coding Starts here. Create a row of data in a variable******/}
+    const rowData = [
+      issue.id,
+      issue.title,
+      issue.status,
+      issue.owner,
+      issue.created.toISOString().split('T')[0], // 格式化日期
+      issue.effort,
+      issue.due ? issue.due.toISOString().split('T')[0] : "N/A",
+  ];
     {/****** Q2: Coding Ends here.******/}
     return (
       <>
       {/****** Q2: Start Coding here. Add Logic to render a row  ******/}
+      <Row data={rowData} style={styles.row} textStyle={styles.text} />
       
       {/****** Q2: Coding Ends here. ******/}  
       </>
@@ -101,14 +112,20 @@ function IssueRow(props) {
     );
 
     {/****** Q2: Start Coding here. Add Logic to initalize table header  ******/}
-
+    const tableHeader = ['ID', 'Title', 'Status', 'Owner', 'Created', 'Effort', 'Due'];
+    
     {/****** Q2: Coding Ends here. ******/}
     
     
     return (
     <View style={styles.container}>
     {/****** Q2: Start Coding here to render the table header/rows.**********/}
-    
+            <Table borderStyle={{ borderWidth: 1, borderColor: '#C1C0B9' }}>
+                <Row data={tableHeader} style={styles.header} textStyle={styles.text} />
+                <ScrollView style={styles.dataWrapper}>
+                    {issueRows}
+                </ScrollView>
+            </Table>
     {/****** Q2: Coding Ends here. ******/}
     </View>
     );
@@ -214,6 +231,7 @@ export default class IssueList extends React.Component {
 
 
     {/****** Q2: Start Coding here. ******/}
+    <IssueTable issues={this.state.issues} />
     {/****** Q2: Code ends here ******/}
 
     
